760c36323f7d18b556ca2f1371b14b21e2ce8bd3
Hasnat-Zakir <hasnatzakir7258@gmail.com>
2026-05-14 03:53:55 +0500

    second commit

---
+++ Basics/Slicing string/script.js
@@ -0,0 +1,12 @@
+// Slicing the string
+
+let fullName = "Hasnat Zakir";
+
+let firstName;
+let lastName;
+
+firstName = fullName.slice(0, 6);
+lastName = fullName.slice(6);
+
+console.log(firstName);
+console.log(lastName);
---
+++ Basics/Strings Methods/script.js
@@ -0,0 +1,11 @@
+// Useful string properties and methods
+
+let userName = "Hasnat Zakir";
+
+console.log(userName.length);
+console.log(userName.charAt(0));
+console.log(userName.indexOf("a"));
+console.log(userName.lastIndexOf("a"));
+
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
