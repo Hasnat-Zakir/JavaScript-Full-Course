93b0b2e7c7b0b33f39bff1b4440587c494f61ff3
Hasnat-Zakir <hasnatzakir7258@gmail.com>
2026-05-15 06:03:50 +0500

    we cover two logics method channing and if statement

---
+++ Basics/Method Channing/script.js
@@ -0,0 +1,9 @@
+// Method channing
+
+let userName = "hasnat";
+
+// let letter = userName.charAt(0);
+// letter = letter.toUpperCase();
+let letter = userName.charAt(0).toUpperCase().trim();
+
+console.log(letter);
---
+++ Basics/if statement/script.js
@@ -0,0 +1,21 @@
+// If Statemetns
+/*
+let age = -5;
+
+if (age >= 18 && age <= 50) {
+  console.log("You are Adult");
+} else if (age > 65) {
+  console.log("You are senior citizen");
+} else if (age < 0) {
+  console.log("You are not born yet");
+} else {
+  console.log("you are not adult");
+}
+*/
+
+let online = false;
+if (online) {
+  console.log("you are online");
+} else {
+  console.log("you are offline");
+}
